9d6bbc71692ca6555d75a6f6f6f80d50d3b65a34
OlgaVino25 <olgavinokyrova25@yandex.ru>
2025-10-23 16:54:05 +0300

    Создается новое сообщение
    новое сообщение с рецептом вместо редактирования существующего

--- bot/commands.py
+++ bot/commands.py
@@ -12,7 +12,7 @@ async def show_recipe(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     recipe = db.get_recipies()[0]
 
-    await query.edit_message_text(
+    await query.message.reply_text(
         strings.show_recipe(recipe),
         reply_markup=get_recipe_keyboard(),
         parse_mode="HTML",
@@ -21,7 +21,7 @@ async def show_recipe(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def show_option2(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
-    await query.edit_message_text(
+    await query.message.reply_text(
         strings.OPTION2, reply_markup=get_main_menu_keyboard(), parse_mode="HTML"
     )
 
@@ -35,7 +35,7 @@ async def show_help(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def back_to_menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
-    await query.edit_message_text(
+    await query.message.reply_text(
         strings.WELCOME_MESSAGE,
         reply_markup=get_main_menu_keyboard(),
         parse_mode="HTML",
