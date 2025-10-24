515fb9f14ef45d390fa2851d1486b87009a3723b
olenevoid <mazekunov@gmail.com>
2025-10-24 16:17:59 +0500

    Закончил предварительное проектирование бд
    Удалил тэги и подписку
    Добавил тип ингредиента и тип порции
    Добавил цены в модели порции и ингредиента

--- demodata/models.py
+++ demodata/models.py
@@ -3,16 +3,26 @@ from datetime import datetime
 
 
 @dataclass
-class Ingredient:
+class PortionType:
     id: int
     title: str
-    portion: str
 
 
 @dataclass
-class Tag:
+class Ingredient:
     id: int
     title: str
+    price: int
+    portion_type: PortionType
+    default_portion_size: int
+
+
+@dataclass
+class IngredientPortion:
+    id: int
+    ingredient: Ingredient
+    portion_size: int
+    portion_price: int #calculated field
 
 
 @dataclass
@@ -20,17 +30,7 @@ class Recipe:
     id: int
     title: str
     instruction: str
-    ingredients: list[Ingredient]
-    tags: list[Tag] | None = None
-
-
-@dataclass
-class Subscription:
-    id: int
-    title: str
-    subscribed_at: datetime
-    subscribed_unti: datetime
-    is_active: bool
+    ingredients: list[IngredientPortion]
 
 
 @dataclass
@@ -38,7 +38,9 @@ class DailyRecipe:
     id: int
     recipe: Recipe | None
     history: list[Recipe]
-    limit: int = 3
+    history_limit: int = 5
+    refresh_limit: int = 3
+    refresh_count: int
     updated_at: datetime | None
     favorite_recipies: list[Recipe] | None
     disliked_recipies: list[Recipe] | None
