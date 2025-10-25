ce60c19fe5879cae981ec2d15c658e318b58d852
olenevoid <mazekunov@gmail.com>
2025-10-25 18:00:21 +0500

    Добавил админку

--- food_plan_app/admin.py
+++ food_plan_app/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import PortionType, Ingredient, IngredientPortion, Recipe, DailyRecipe, User
 
-# Register your models here.
+
+#admin.site.register(PortionType)
+#admin.site.register(IngredientPortion)
+#admin.site.register(DailyRecipe)
+#admin.site.register(User)
+
+
+@admin.register(PortionType)
+class PortionTypeAdmin(admin.ModelAdmin):
+    list_display = ['title', 'title_short']
+    search_fields = ['title', 'title_short']
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'portion_type', 'default_portion_size']
+    list_filter = ['portion_type']
+    search_fields = ['title']
+    list_select_related = ['portion_type']
+
+
+class IngredientPortionInline(admin.TabularInline):
+    model = IngredientPortion
+    extra = 1
+    fields = ['ingredient', 'portion_size', 'portion_type', 'portion_price']
+    readonly_fields = ['portion_price', 'portion_type']
+
+    def portion_type(self, obj):
+        return obj.ingredient.portion_type
+
+    def portion_price(self, obj):
+        return obj.portion_price
+
+    portion_type.short_description = 'Единица измерения'
+    portion_price.short_description = 'Цена'
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ['title', 'image']
+    search_fields = ['title']
+    inlines = [IngredientPortionInline]
