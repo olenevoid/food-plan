872c54f94209feef6385d5c84278bc8ae7d900d5
olenevoid <mazekunov@gmail.com>
2025-10-27 11:17:35 +0500

    Добавил путь к папке media в настройки

--- food_plan/settings.py
+++ food_plan/settings.py
@@ -19,6 +19,7 @@ env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Quick-start development settings - unsuitable for production
